112f95a2ddb3267803befff362f7a19d5cee632d
Present-truth <presenttrouble@gmail.com>
2025-01-27 09:16:16 +0100

    Update TalkTo.js

--- TalkTo.js
+++ TalkTo.js
@@ -1,11 +1,36 @@
-const imaging = document.querySelector('.div-one');
-const images = ['two', 'one', 'download', 'three'];
-let currentImage = 0;
-setInterval(() => {
-    imaging.classList.remove(...images);
-    imaging.classList.add(images[currentImage]);
-    currentImage = (currentImage + 1) % images.lenght;
+// Define an arrayof images sources
 
-}, 10000);
 
-image.classList.add(images[0]);
+const images = [
+    "./Pictures/test-five.jpeg",
+    "./Pictures/temp-five.png",
+];
+
+// Get the image element
+const imageElement = document.querySelector(".play-time");
+
+// Initialize the image index
+let imageIndex = 0
+
+
+// now to function the image element
+function changeImage() {
+
+    // Update the function 
+    imageElement.src = images[imageIndex];
+
+
+    // Increment the image index 
+    imageIndex = (imageIndex + 1) % images.lenght;
+}
+
+
+// Call the changeImage function every 3 seconds
+
+setInterval(changeImage, 3000);
+
+
+
+//Continue looping it more
+
+changeImage();
